2865a06428de37f907c207d6e39f80a31c26bbc5
CasDevSilva <sca.dev.silva@gmail.com>
2025-08-13 21:32:04 -0500

    struct: Note model interface

---
+++ src/models/Note.ts
@@ -0,0 +1,9 @@
+export interface Note {
+    name: string,
+    content: string,
+    category_id: number,
+    extension: string,
+    status: string,
+    date_created: string,
+    date_updated: string
+}
