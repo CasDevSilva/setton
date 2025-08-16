5f0aaa5492f28a34c3de8cb0382e6d7b0c221726
CasDevSilva <sca.dev.silva@gmail.com>
2025-08-16 12:14:09 -0500

    dev: Console messages custom

--- src/lib/commands/config.ts
+++ src/lib/commands/config.ts
@@ -4,13 +4,14 @@ import { existFile } from "../utils/stats.js";
 export async function config() {
     try {
         let mObjStats = await existFile(dbFile);
+        let mIntTtlColumns = process.stdout.columns || 80;
 
-        console.log("==============================================")
+        console.log("Información de Base de Datos".padStart(mIntTtlColumns, "="))
         console.log("Path File: ", dbFile);
         console.log("Size: ", mObjStats.size);
         console.log("Date created: ", new Date(mObjStats.birthtime).toUTCString());
         console.log("Last updated: ", new Date(mObjStats.mtime).toUTCString());
-        console.log("==============================================")
+        console.log("=".repeat(mIntTtlColumns));
     } catch(err) {
         console.log("Error al obtener informacion de la Base de Datos.");
     }
