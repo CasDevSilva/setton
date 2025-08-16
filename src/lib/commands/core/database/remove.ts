d4d8502e9a2c7760b8971ac11f591ead23928f5b
CasDevSilva <sca.dev.silva@gmail.com>
2025-08-16 12:09:31 -0500

    dev: command remove (database)

---
+++ src/lib/commands/core/database/remove.ts
@@ -0,0 +1,30 @@
+import { archiveNote, getNotes } from "../../../utils/tables/notes.js";
+import inquirer from "inquirer";
+import inquirerPrompt from 'inquirer-autocomplete-prompt';
+
+inquirer.registerPrompt('autocomplete', inquirerPrompt);
+
+export async function removeDBNote() {
+    try {
+        let mArrObjNotes = getNotes();
+        let mObjDataNote = await inquirer.prompt([
+            {
+                name: 'name',
+                type: 'autocomplete',
+                message: 'Name of your note: ',
+                source: (answers: any, input:string) => {
+                    input = input || '';
+                    return mArrObjNotes.filter(mRowNote => mRowNote.name.toLowerCase().includes(input.toLowerCase()))
+                }
+            }
+        ]);
+
+        let mObjNote = mArrObjNotes.find(mRowNote => mRowNote.name.toLowerCase().includes(
+            mObjDataNote.name.toLowerCase()
+        ));
+
+        archiveNote(mObjNote.id, true);
+    } catch(err) {
+        console.log("Error al eliminar la nota de la Base de Datos.");
+    }
+}
